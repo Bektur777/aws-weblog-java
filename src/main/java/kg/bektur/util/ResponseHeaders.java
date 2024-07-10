ac5c54629f7f8860d78f33be4b5db10d12885125
Bektur777 <ubei003@mail.ru>
2024-07-10 18:04:40 +0600

    Add response headers to post method

--- src/main/java/kg/bektur/util/ResponseHeaders.java
+++ src/main/java/kg/bektur/util/ResponseHeaders.java
@@ -7,10 +7,17 @@ public class ResponseHeaders {
 
     public static final Map<String, String> DEFAULT_GET_HEADERS;
 
+    public static final Map<String, String> DEFAULT_POST_HEADERS;
+
     static {
         DEFAULT_GET_HEADERS = new HashMap<>();
         DEFAULT_GET_HEADERS.put("Content-Type", "application/json");
         DEFAULT_GET_HEADERS.put("Access-Control-Allow-Origin", "*");
         DEFAULT_GET_HEADERS.put("Access-Control-Allow-Methods", "GET");
+
+        DEFAULT_POST_HEADERS = new HashMap<>();
+        DEFAULT_GET_HEADERS.put("Content-Type", "application/json");
+        DEFAULT_GET_HEADERS.put("Access-Control-Allow-Origin", "*");
+        DEFAULT_GET_HEADERS.put("Access-Control-Allow-Methods", "POST");
     }
 }
