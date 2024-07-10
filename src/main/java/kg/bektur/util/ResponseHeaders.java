f74307b0fc37d9caebeebd4f14b97aaa211d941d
Bektur777 <ubei003@mail.ru>
2024-07-10 19:23:36 +0600

    Add default delete headers

--- src/main/java/kg/bektur/util/ResponseHeaders.java
+++ src/main/java/kg/bektur/util/ResponseHeaders.java
@@ -9,6 +9,8 @@ public class ResponseHeaders {
 
     public static final Map<String, String> DEFAULT_POST_HEADERS;
 
+    public static final Map<String, String> DEFAULT_DELETE_HEADERS;
+
     static {
         DEFAULT_GET_HEADERS = new HashMap<>();
         DEFAULT_GET_HEADERS.put("Content-Type", "application/json");
@@ -19,5 +21,10 @@ public class ResponseHeaders {
         DEFAULT_GET_HEADERS.put("Content-Type", "application/json");
         DEFAULT_GET_HEADERS.put("Access-Control-Allow-Origin", "*");
         DEFAULT_GET_HEADERS.put("Access-Control-Allow-Methods", "POST");
+
+        DEFAULT_DELETE_HEADERS = new HashMap<>();
+        DEFAULT_DELETE_HEADERS.put("Content-Type", "application/json");
+        DEFAULT_DELETE_HEADERS.put("Access-Control-Allow-Origin", "*");
+        DEFAULT_DELETE_HEADERS.put("Access-Control-Allow-Methods", "DELETE");
     }
 }
