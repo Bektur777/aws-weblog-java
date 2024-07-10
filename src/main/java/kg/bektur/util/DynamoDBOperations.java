1614243226299a1a1c7330a86603d57e615bbf0e
Bektur777 <ubei003@mail.ru>
2024-07-10 18:48:12 +0600

    Create dynamodb operation

---
+++ src/main/java/kg/bektur/util/DynamoDBOperations.java
@@ -0,0 +1,12 @@
+package kg.bektur.util;
+
+public class DynamoDBOperations {
+
+    public static final String SCAN_ITEMS = "dynamodb:Scan";
+
+    public static final String GET_ITEM = "dynamodb:GetItem";
+
+    public static final String PUT_ITEM = "dynamodb:PutItem";
+
+    public static final String DELETE_ITEM = "dynamodb:DeleteItem";
+}
