d8a60a684023a9f1844194bc9a291abf04d8878d
Bektur777 <ubei003@mail.ru>
2024-07-10 17:05:40 +0600

    Create PostService

---
+++ src/main/java/kg/bektur/service/PostService.java
@@ -0,0 +1,8 @@
+package kg.bektur.service;
+
+import kg.bektur.dto.PostCreateDto;
+import kg.bektur.dto.PostDto;
+
+public interface PostService extends DefaultService<PostDto, PostCreateDto> {
+    void create(PostCreateDto postCreateDto);
+}
